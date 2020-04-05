6da922b475d212ef0fb1582a75782e486cb54c5a
tobiasjaster <51821729+tobiasjaster@users.noreply.github.com>
2020-04-05 18:55:48 +0000

    Update

--- createStammtischServer.sh
+++ createStammtischServer.sh
@@ -1,18 +1,204 @@
-#! /bin/sh
+#! /bin/bash
 
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install openssh-server openvpn wget
-sudo systemctl start sshd
+if [ -z $1 ]; then
+    echo "Please Input Password of 7z Archive"
+    read password
+else
+    password=$1
+fi
+ 	
+apt-get update
+apt-get upgrade
+apt-get install -y openssh-server openvpn
 
-cd Downloads
+mkdir -p ~/.ssh
+cat <<EOF > ~/.ssh/authorized_keys
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCkj7GlLHP0JT+Y1hFYP0df3AZ4Yx8rDNdS8b3yA3R+FBDN9nspa6P9EaPc+ot+rgP1FvGofxlN+v1g0RKDXEVEL7p7acoTeo790EtjPu1ddufQOcZgba9p0/T9iqRwBuw1fRAIzgkRuOH9JbYXeqd9cu/AeIWHZuAziN9mZhjmWDOwKQfvyFZYdQzOgDHKEDhgiy9MlfLzF3m4OgGWZJFdn/BoTs/xmUBjV0Ue+X2JyDGKvHmEzKaxnzHvDzq28DCyJKZXfKQ+SJLXlK/CqA4UHYu3rDL5IYORZHlBb0QEekkzXK1WzYVqe4T0OWmRfrCgvHwJUa9Ex9r0awUgmBVh
+EOF
 
-curl -H 'Authorization: token  341f3cfb08bd0e95d8ea63fe5515e1f87ef4c573' \
-  -H 'Accept: application/vnd.github.v4.raw' \
-  -O \
-  -L https://github.com/tobiasjaster/wireguard/raw/master/openvpn.tar.7z
-7z x openvpn.tar.7z -pKSGStammtisch1!
-sudo mv openvpn.tar /etc/openvpn/
+cat <<EOF > /etc/ssh/sshd_config
+#	$OpenBSD: sshd_config,v 1.103 2018/04/09 20:41:22 tj Exp $
+
+# This is the sshd server system-wide configuration file.  See
+# sshd_config(5) for more information.
+
+# This sshd was compiled with PATH=/usr/local/sbin:/usr/local/bin:/usr/bin
+
+# The strategy used for options in the default sshd_config shipped with
+# OpenSSH is to specify options with their default value where
+# possible, but leave them commented.  Uncommented options override the
+# default value.
+
+#Port 22
+#AddressFamily any
+#ListenAddress 0.0.0.0
+#ListenAddress ::
+
+#HostKey /etc/ssh/ssh_host_rsa_key
+#HostKey /etc/ssh/ssh_host_ecdsa_key
+#HostKey /etc/ssh/ssh_host_ed25519_key
+
+# Ciphers and keying
+#RekeyLimit default none
+
+# Logging
+#SyslogFacility AUTH
+#LogLevel INFO
+
+# Authentication:
+
+#LoginGraceTime 2m
+#PermitRootLogin prohibit-password
+#StrictModes yes
+#MaxAuthTries 6
+#MaxSessions 10
+
+#PubkeyAuthentication yes
+
+# The default is to check both .ssh/authorized_keys and .ssh/authorized_keys2
+# but this is overridden so installations will only check .ssh/authorized_keys
+AuthorizedKeysFile	.ssh/authorized_keys
+
+#AuthorizedPrincipalsFile none
+
+#AuthorizedKeysCommand none
+#AuthorizedKeysCommandUser nobody
+
+# For this to work you will also need host keys in /etc/ssh/ssh_known_hosts
+#HostbasedAuthentication no
+# Change to yes if you don't trust ~/.ssh/known_hosts for
+# HostbasedAuthentication
+#IgnoreUserKnownHosts no
+# Don't read the user's ~/.rhosts and ~/.shosts files
+#IgnoreRhosts yes
+
+# To disable tunneled clear text passwords, change to no here!
+PasswordAuthentication no
+#PermitEmptyPasswords no
+
+# Change to no to disable s/key passwords
+ChallengeResponseAuthentication no
+
+# Kerberos options
+#KerberosAuthentication no
+#KerberosOrLocalPasswd yes
+#KerberosTicketCleanup yes
+#KerberosGetAFSToken no
+
+# GSSAPI options
+#GSSAPIAuthentication no
+#GSSAPICleanupCredentials yes
+
+# Set this to 'yes' to enable PAM authentication, account processing,
+# and session processing. If this is enabled, PAM authentication will
+# be allowed through the ChallengeResponseAuthentication and
+# PasswordAuthentication.  Depending on your PAM configuration,
+# PAM authentication via ChallengeResponseAuthentication may bypass
+# the setting of "PermitRootLogin without-password".
+# If you just want the PAM account and session checks to run without
+# PAM authentication, then enable this but set PasswordAuthentication
+# and ChallengeResponseAuthentication to 'no'.
+UsePAM no
+
+#AllowAgentForwarding yes
+#AllowTcpForwarding yes
+#GatewayPorts no
+X11Forwarding yes
+#X11DisplayOffset 10
+#X11UseLocalhost yes
+#PermitTTY yes
+PrintMotd no # pam does that
+#PrintLastLog yes
+#TCPKeepAlive yes
+#PermitUserEnvironment no
+#Compression delayed
+#ClientAliveInterval 0
+#ClientAliveCountMax 3
+#UseDNS no
+#PidFile /run/sshd.pid
+#MaxStartups 10:30:100
+#PermitTunnel no
+#ChrootDirectory none
+#VersionAddendum none
+
+# no default banner path
+#Banner none
+
+# override default of no subsystems
+Subsystem	sftp	/usr/lib/ssh/sftp-server
+
+# Example of overriding settings on a per-user basis
+#Match User anoncvs
+#	X11Forwarding no
+#	AllowTcpForwarding no
+#	PermitTTY no
+#	ForceCommand cvs server
+EOF
+systemctl start sshd
 
 cd /etc/openvpn
+curl -O -L https://github.com/tobiasjaster/wireguard/raw/master/openvpn.tar.7z
+7z x openvpn.tar.7z -p$password
+rm openvpn.tar.7z
 tar -xf openvpn.tar
+rm openvpn.tar
+cat <<EOF > /lib/systemd/system/openvpn.service
+# This service is actually a systemd target,
+# but we are using a service since targets cannot be reloaded.
+
+[Unit]
+Description=OpenVPN service
+After=network.target
+
+[Service]
+Type=oneshot
+RemainAfterExit=yes
+ExecStart=/bin/true
+ExecReload=/bin/true
+WorkingDirectory=/etc/openvpn
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo cat <<EOF > /lib/systemd/system/openvpn@.service
+[Unit]
+Description=OpenVPN connection to %i
+PartOf=openvpn.service
+ReloadPropagatedFrom=openvpn.service
+Before=systemd-user-sessions.service
+After=network-online.target
+Wants=network-online.target
+Documentation=man:openvpn(8)
+Documentation=https://community.openvpn.net/openvpn/wiki/Openvpn24ManPage
+Documentation=https://community.openvpn.net/openvpn/wiki/HOWTO
+
+[Service]
+Type=notify
+PrivateTmp=true
+WorkingDirectory=/etc/openvpn
+ExecStart=/usr/sbin/openvpn --daemon ovpn-%i --status /run/openvpn/%i.status 10 --cd /etc/openvpn --config /etc/openvpn/%i.conf --writepid /run/openvpn/%i.pid
+PIDFile=/run/openvpn/%i.pid
+KillMode=process
+ExecReload=/bin/kill -HUP $MAINPID
+CapabilityBoundingSet=CAP_IPC_LOCK CAP_NET_ADMIN CAP_NET_BIND_SERVICE CAP_NET_RAW CAP_SETGID CAP_SETUID CAP_SYS_CHROOT CAP_DAC_OVERRIDE CAP_AUDIT_WRITE
+LimitNPROC=100
+DeviceAllow=/dev/null rw
+DeviceAllow=/dev/net/tun rw
+ProtectSystem=true
+ProtectHome=true
+RestartSec=5s
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+useradd openvpn
+groupadd openvpn
+usermod -a -G openvpn openvpn
+
+systemctl enable openvpn.service
+systemctl enable openvpn@server.service
+systemctl start openvpn.service
+systemctl start openvpn@server.service
+
